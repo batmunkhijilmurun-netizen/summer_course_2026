51204f2ecef10a9c7ffea18f307ef62b263b16c0
Summer <summer@admin-05s-iMac.local>
2026-08-25 10:58:18 +0800

    week8 tuesday

--- my-firebase-workspace/projects/project03/sketch.js
+++ my-firebase-workspace/projects/project03/sketch.js
@@ -3,10 +3,25 @@ let bubbles = [];
 let rocks = [];
 let seaweeds = [];
 let shark = null;
+let octopus = null;
+let giantClam = null;
+let bgImage = null;
+let bgW = 0;
+let bgH = 0;
+
+// Зургийг ачаалах
+function preload() {
+  bgImage = loadImage('screenshot/us.jpg');
+}
 
 function setup() {
   let canvas = createCanvas(1500, 900);
   canvas.parent('canvas-container');
+
+  // Зургийг бүхэл canvas-ийг бүрхүүлэхээр масштаблах (cover)
+  let scale = max(width / bgImage.width, height / bgImage.height);
+  bgW = bgImage.width * scale;
+  bgH = bgImage.height * scale;
 
   for (let i = 0; i < 15; i++) {
     rocks.push({
@@ -56,17 +71,41 @@ function setup() {
     speedX: 3,
     speedY: 2
   };
+
+  // Наймаалжны анхны байрлал
+  octopus = {
+    x: width * 0.25,
+    y: height - 120,
+    size: 70,
+    speedX: 1.5,
+    offset: 0
+  };
+
+  // Том дун хясааны байрлал (Элсэн дээр байрлана)
+  giantClam = {
+    x: width * 0.75,
+    y: height - 60,
+    w: 140,
+    h: 90
+  };
 }
 
 function draw() {
-  background('#32409cff');
+  // Зурагтай background (cover)
+  image(bgImage, (width - bgW) / 2, (height - bgH) / 2, bgW, bgH);
   drawSeaweeds();
 
+  // Элс / Далайн ёроол
   noStroke();
   fill(218, 183, 111);
   rect(0, height - 60, width, 60);
 
   drawRocks();
+
+  // Том хясаа болон наймаалжийг зурах
+  drawGiantClam(giantClam);
+  drawOctopus(octopus);
+  moveOctopus(octopus);
 
   for (let b of bubbles) {
     drawBubble(b);
@@ -109,6 +148,85 @@ function drawRocks() {
   for (let r of rocks) {
     fill(r.color);
     ellipse(r.x, r.y, r.w, r.h);
+  }
+}
+
+// Том хясаа зурах функц (Нээгдэж, хаагдах хөдөлгөөнтэй + Сувдтай)
+function drawGiantClam(clam) {
+  push();
+  translate(clam.x, clam.y);
+
+  // Хясааны нээгдэх өнцгийн хөдөлгөөн
+  let openAngle = sin(frameCount * 0.02) * 0.25 + 0.15;
+
+  // Доод таг
+  fill(180, 140, 190);
+  stroke(100, 60, 110);
+  strokeWeight(3);
+  arc(0, 0, clam.w, clam.h, 0, PI, CHORD);
+
+  // Доторх сувд
+  noStroke();
+  fill(255, 245, 230);
+  ellipse(0, -5, 22, 22);
+  fill(255);
+  ellipse(-3, -8, 7, 7);
+
+  // Дээд таг (Нээгдэж, хаагдана)
+  push();
+  translate(0, -5);
+  rotate(-openAngle);
+  stroke(100, 60, 110);
+  strokeWeight(3);
+  fill(200, 150, 210);
+  arc(0, 0, clam.w, clam.h, PI, TWO_PI, CHORD);
+  pop();
+
+  pop();
+}
+
+// Наймаалж зурах функц
+function drawOctopus(oct) {
+  push();
+  translate(oct.x, oct.y);
+
+  // Наймаалжны хөлнүүд (Давалгаалах хөдөлгөөнтэй)
+  stroke(220, 80, 110);
+  strokeWeight(8);
+  noFill();
+  for (let i = -3; i <= 3; i++) {
+    beginShape();
+    let startX = i * 8;
+    for (let j = 0; j < 40; j += 10) {
+      let wave = sin(frameCount * 0.1 + i + j * 0.1) * 8;
+      vertex(startX + wave, j);
+    }
+    endShape();
+  }
+
+  // Наймаалжны толгой
+  noStroke();
+  fill(230, 90, 120);
+  ellipse(0, -10, oct.size, oct.size * 0.8);
+
+  // Нүднүүд
+  fill(255);
+  ellipse(-12, -12, 14, 14);
+  ellipse(12, -12, 14, 14);
+  fill(0);
+  ellipse(-12, -12, 6, 6);
+  ellipse(12, -12, 6, 6);
+
+  pop();
+}
+
+// Наймаалжны хөдөлгөөн
+function moveOctopus(oct) {
+  oct.x += oct.speedX;
+  oct.y += sin(frameCount * 0.05) * 0.5; // Бага зэрэг дээш доош хөвнө
+
+  if (oct.x > width - 100 || oct.x < 100) {
+    oct.speedX *= -1;
   }
 }
 
@@ -155,39 +273,32 @@ function drawShark(x, y, size, speedX) {
     scale(-1, 1);
   }
 
-  // Бие
   fill(120, 140, 160);
   noStroke();
   ellipse(0, 0, size * 1.3, size * 0.6);
 
-  // Сүүл
   triangle(
     -size * 0.6, 0,
     -size * 0.9, -size * 0.3,
     -size * 0.9, size * 0.3
   );
 
-  // Дээд сүүс
   triangle(
     -size * 0.1, -size * 0.25,
     size * 0.1, -size * 0.25,
     -size * 0.1, -size * 0.6
   );
 
-  // Нүд
   fill(255);
   ellipse(size * 0.35, -size * 0.15, size * 0.15, size * 0.15);
   fill(0);
   ellipse(size * 0.37, -size * 0.15, size * 0.07, size * 0.07);
 
-  // Амын хөдөлгөөн (ангалзах эффект)
   let mouthOpen = abs(sin(frameCount * 0.1)) * (size * 0.12);
 
-  // Нээгдсэн ам (дөнгөж фоны өнгөөр зүсэж харуулна)
-  fill('#32409cff');
+  noFill();
   triangle(size * 0.35, 0, size * 0.68, -mouthOpen, size * 0.68, mouthOpen);
 
-  // Дээд ба доод шүднүүд
   fill(255);
   triangle(size * 0.45, -mouthOpen + 2, size * 0.5, 0, size * 0.55, -mouthOpen + 2);
   triangle(size * 0.45, mouthOpen - 2, size * 0.5, 0, size * 0.55, mouthOpen - 2);
@@ -196,15 +307,25 @@ function drawShark(x, y, size, speedX) {
 }
 
 function moveShark(s) {
-  s.x += s.speedX;
-  s.y += s.speedY;
+  let moveSpeed = 4; 
 
-  if (s.x + s.size / 2 > width || s.x - s.size / 2 < 0) {
-    s.speedX *= -1;
+  if (keyIsDown(87) || keyIsDown(119)) {
+    s.y -= moveSpeed;
   }
-  if (s.y + s.size / 4 > height - 60 || s.y - s.size / 4 < 0) {
-    s.speedY *= -1;
+  if (keyIsDown(83) || keyIsDown(115)) {
+    s.y += moveSpeed;
   }
+  if (keyIsDown(65) || keyIsDown(97)) {
+    s.x -= moveSpeed;
+    s.speedX = -1;
+  }
+  if (keyIsDown(68) || keyIsDown(100)) {
+    s.x += moveSpeed;
+    s.speedX = 1;
+  }
+
+  s.x = constrain(s.x, s.size / 2, width - s.size / 2);
+  s.y = constrain(s.y, s.size / 4, height - 60);
 }
 
 function checkSharkEatFishes() {
@@ -254,30 +375,4 @@ function mousePressed() {
       });
     }
   }
-}
-function moveShark(s) {
-  let moveSpeed = 4; // Акулын шилжих хурд
-
-  // W / w - Дээш
-  if (keyIsDown(87) || keyIsDown(119)) {
-    s.y -= moveSpeed;
-  }
-  // S / s - Доош
-  if (keyIsDown(83) || keyIsDown(115)) {
-    s.y += moveSpeed;
-  }
-  // A / a - Зүүн
-  if (keyIsDown(65) || keyIsDown(97)) {
-    s.x -= moveSpeed;
-    s.speedX = -1;
-  }
-  // D / d - Баруун
-  if (keyIsDown(68) || keyIsDown(100)) {
-    s.x += moveSpeed;
-    s.speedX = 1;
-  }
-
-  // Дэлгэцээс гаргахгүй байх хязгаарлалт
-  s.x = constrain(s.x, s.size / 2, width - s.size / 2);
-  s.y = constrain(s.y, s.size / 4, height - 60);
 }
